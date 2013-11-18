b87282c7771702e363471ba896d482877d339680
kubo <jakub.jursa@relbit.com>
2013-11-18 11:39:44 +0100

    code style fix

--- lib/bitstat/cli.rb
+++ lib/bitstat/cli.rb
@@ -139,14 +139,14 @@ module Bitstat
     #end
 
     def daemonize
-      if RUBY_VERSION < "1.9"
+      if RUBY_VERSION < '1.9'
         exit if fork
         Process.setsid
         exit if fork
-        Dir.chdir "/"
-        STDIN.reopen "/dev/null"
-        STDOUT.reopen "/dev/null", "a"
-        STDERR.reopen "/dev/null", "a"
+        Dir.chdir('/')
+        STDIN.reopen('/dev/null')
+        STDOUT.reopen('/dev/null', 'a')
+        STDERR.reopen('/dev/null', 'a')
       else
         Process.daemon
       end
