c9780aacbe27a578a1d50a222b66965895eb9dca
kubo <jakub.jursa@relbit.com>
2014-03-06 13:59:15 +0100

    symbolize_string_keys refactored

--- lib/bitstat/core_ext/object.rb
+++ lib/bitstat/core_ext/object.rb
@@ -1,10 +1,16 @@
 class Object
-  def symbolize_string_keys
+  def symbolize_string_keys(visited = Set.new)
+    if visited.include? self.object_id
+      raise "Universe has just broken down, sad lolcat. #{self.to_yaml}"
+    end
+
     if self.is_a?(Array)
-      self.map(&:symbolize_string_keys)
+      visited << self.object_id
+      self.map { |a| a.symbolize_string_keys visited }
     elsif self.is_a?(Hash)
+      visited << self.object_id
       self.inject({}) do |new_hash, (k, v)|
-        new_hash[k.is_a?(String) ? k.to_sym : k] = v.symbolize_string_keys
+        new_hash[k.is_a?(String) ? k.to_sym : k] = v.symbolize_string_keys(visited)
         new_hash
       end
     else
